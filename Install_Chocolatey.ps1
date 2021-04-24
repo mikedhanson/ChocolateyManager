68c0379690a0464e7eec3c03c32938579dcfa309
Michael Hanson <15201245+mikedhanson@users.noreply.github.com>
2021-04-24 12:19:26 -0500

    Update Install_Chocolatey.ps1

--- Install_Chocolatey.ps1
+++ Install_Chocolatey.ps1
@@ -1,3 +1,26 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
+$VerbosePreference = 'Continue'
+if (-not $env:ChocolateyInstall) {
+    $message = @(
+        "The ChocolateyInstall environment variable was not found."
+        "Chocolatey is not detected as installed. Nothing to do."
+    ) -join "`n"
+
+    Write-Warning $message
+    $message | Out-File -FilePath $logFile -Force -Append 
+    return
+}
+
+if (-not (Test-Path $env:ChocolateyInstall)) {
+    $message = @(
+        "No Chocolatey installation detected at '$env:ChocolateyInstall'."
+        "Nothing to do."
+    ) -join "`n"
+
+    Write-Warning $message
+    $message | Out-File -FilePath $logFile -Force -Append
+    return
+}
+
 invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))

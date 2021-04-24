5177bae4ce53aab33559cc4f7cde57e5d007226b
Michael Hanson <15201245+mikedhanson@users.noreply.github.com>
2021-04-24 12:30:50 -0500

    Update Install_Chocolatey.ps1
    Fixed

--- Install_Chocolatey.ps1
+++ Install_Chocolatey.ps1
@@ -1,26 +1,18 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
-$VerbosePreference = 'Continue'
-if (-not $env:ChocolateyInstall) {
-    $message = @(
-        "The ChocolateyInstall environment variable was not found."
-        "Chocolatey is not detected as installed. Nothing to do."
-    ) -join "`n"
+$PackageName = "ChocolateyManager"
+$ChocoManagerPath = "C:\Users\$env:USERNAME\Appdata\Local"
+$logFile = "$ChocoManagerPath\$PackageName\log.txt"
 
+if(!(Test-Path -Path "$ChocoManagerPath\$PackageName")){
+    New-Item -ItemType Directory -Name $PackageName -Path $ChocoManagerPath -Force
+}
+
+$VerbosePreference = 'Continue'
+if ($env:ChocolateyInstall) {
+    $message = @("Chocolatey was found on the system. Nothing to do" ) -join "`n"
     Write-Warning $message
     $message | Out-File -FilePath $logFile -Force -Append 
-    return
+}else {
+    invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-
-if (-not (Test-Path $env:ChocolateyInstall)) {
-    $message = @(
-        "No Chocolatey installation detected at '$env:ChocolateyInstall'."
-        "Nothing to do."
-    ) -join "`n"
-
-    Write-Warning $message
-    $message | Out-File -FilePath $logFile -Force -Append
-    return
-}
-
-invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))

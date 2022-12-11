6ce6b6d2a54881b4c5f67f1d881e3ac630352c89
Michael Hanson <15201245+mikedhanson@users.noreply.github.com>
2022-12-11 09:43:39 -0600

    Update Choco_Update.ps1
    Adding windows toast when updates happen

--- Choco_Update.ps1
+++ Choco_Update.ps1
@@ -1,20 +1,53 @@
 <#
-.SYNOPSIS
-Short description
+    .SYNOPSIS
+    Short description
 
-.DESCRIPTION
-Long description
+    .DESCRIPTION
+    Long description
 
-.EXAMPLE
-An example
+    .EXAMPLE
+    An example
 
-.NOTES
-General notes
+    .NOTES
+    General notes
 #>
 
 $PackageName = "ChocolateyManager"
 $ChocoManagerPath = "C:\Users\$env:USERNAME\Appdata\Local"
 $logFile = "$ChocoManagerPath\$PackageName\log.txt"
+
+function Show-Notification {
+
+    [cmdletbinding()]
+    Param (
+        [string]
+        $ToastTitle,
+
+        [string]
+        [parameter(ValueFromPipeline)]
+        $ToastText
+    )
+
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+    $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+
+    $RawXml = [xml] $Template.GetXml()
+    ($RawXml.toast.visual.binding.text | where-object {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
+    ($RawXml.toast.visual.binding.text | where-object {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+
+    $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
+    $SerializedXml.LoadXml($RawXml.OuterXml)
+
+    $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
+    $Toast.Tag = "PowerShell"
+    $Toast.Group = "PowerShell"
+    $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+    $Notifier.Show($Toast);
+}
+
+Show-Notification -ToastTitle $PackageName -ToastText "Upgarding apps. Log file can be found here: $logFile"
 
 $UpdateOutput = choco upgrade all --yes 
 
